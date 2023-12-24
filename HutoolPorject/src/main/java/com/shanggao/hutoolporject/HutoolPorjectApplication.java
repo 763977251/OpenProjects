3584644bb0e2c03277992b7931dc32833106cc19
763977251 <763977251@qq.com>
2023-12-24 13:31:45 +0800

    增加HutoolProject项目模块

---
+++ HutoolPorject/src/main/java/com/shanggao/hutoolporject/HutoolPorjectApplication.java
@@ -0,0 +1,13 @@
+package com.shanggao.hutoolporject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HutoolPorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HutoolPorjectApplication.class, args);
+	}
+
+}
