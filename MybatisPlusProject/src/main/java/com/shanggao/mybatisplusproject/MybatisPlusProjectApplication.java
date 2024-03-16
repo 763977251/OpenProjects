4b26f5283552469a81c2cc403c44714786f9907e
763977251 <763977251@qq.com>
2024-03-16 19:51:20 +0800

    新建项目MybatisPlusProject

---
+++ MybatisPlusProject/src/main/java/com/shanggao/mybatisplusproject/MybatisPlusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shanggao.mybatisplusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusProjectApplication.class, args);
+	}
+
+}
---
+++ MybatisPlusProject/src/test/java/com/shanggao/mybatisplusproject/MybatisPlusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanggao.mybatisplusproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
