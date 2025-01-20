989290d7080b38cc83959448a8243fdba05742ef
尚高 <shanggao@boe.com.cn>
2025-01-20 09:15:58 +0800

    删除不必要的注释

--- HutoolPorject/src/test/java/com/shanggao/hutoolporject/HutoolPorjectApplicationTests.java
+++ HutoolPorject/src/test/java/com/shanggao/hutoolporject/HutoolPorjectApplicationTests.java
@@ -30,16 +30,6 @@ class HutoolPorjectApplicationTests {
 		int index = 1;
 		for (Map<String, Object> map : list) {
 			try {
-				//String siteName = GenericServiceProxy.getConstantMap().SiteName_XY;
-				//String userNumber = in.getString("USERNUMBER");//用户账号
-				//String boxId = in.getString("BOXID"); //箱号
-				//String wipEntityName = in.getString("WIPENTITYNAME"); //任务单号，多个用逗号隔开
-				//String groupCode = in.getString("GROUPCODE"); //班组
-				//BigDecimal consumeQty = in.getBigDecimal("CONSUMEQTY"); //本次消耗数量
-				//String processCode = in.getString("PROCESSCODE"); //工序编码
-				//String lineName = in.getString("LINENAME");
-				//String optimizationNo = in.getString("OPTIMIZATIONNO"); //优化包号
-				//String optimizationNoSeq = in.getString("OPTIMIZATIONNOSEQ"); //优化包版图号
 				Map<String, Object> paramMap = new HashMap<>();
 				paramMap.put("PROCESSCODE", map.get("PROCESSCODE"));
 				paramMap.put("USERNUMBER", map.get("USERNUMBER"));
@@ -51,8 +41,6 @@ class HutoolPorjectApplicationTests {
 				paramMap.put("OPTIMIZATIONNO", map.get("OPTIMIZATIONNO"));
 				paramMap.put("OPTIMIZATIONNOSEQ", map.get("OPTIMIZATIONNOSEQ"));
 				String body = HttpUtil.post("URL", JSONUtil.toJsonStr(paramMap));
-//				String body = HttpRequest.of("http://127.0.0.1:17878/materialpick/materialConsumeReport")
-//						.body(JSONUtil.toJsonStr(paramMap)).execute().body();
 				System.out.println(index++ + body);
 			} catch (Exception e) {
 				e.printStackTrace();
